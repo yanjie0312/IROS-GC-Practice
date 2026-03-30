4b3c85eddd43b1f9d19331aaee6f0ed24995f55c
Yao <e1349011@u.nus.edu>
2026-03-30 20:46:23 +0800

    fix display panel

--- gym_pybullet_drones/envs/BaseAviary.py
+++ gym_pybullet_drones/envs/BaseAviary.py
@@ -148,7 +148,9 @@ class BaseAviary(gym.Env):
         if self.GUI:
             #### With debug GUI ########################################
             self.CLIENT = p.connect(p.GUI) # p.connect(p.GUI, options="--opengl2")
-            for i in [p.COV_ENABLE_RGB_BUFFER_PREVIEW, p.COV_ENABLE_DEPTH_BUFFER_PREVIEW, p.COV_ENABLE_SEGMENTATION_MARK_PREVIEW]:
+            # Enable RGB preview in the left panel; disable depth and segmentation
+            p.configureDebugVisualizer(p.COV_ENABLE_RGB_BUFFER_PREVIEW, 1, physicsClientId=self.CLIENT)
+            for i in [p.COV_ENABLE_DEPTH_BUFFER_PREVIEW, p.COV_ENABLE_SEGMENTATION_MARK_PREVIEW]:
                 p.configureDebugVisualizer(i, 0, physicsClientId=self.CLIENT)
             p.resetDebugVisualizerCamera(cameraDistance=3,
                                          cameraYaw=-30,
